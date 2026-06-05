a59d6cdb74c5ea7524d98fdea2d4442cb7bdad7e
Lakshay Singh <ghanshyamsingh54938@gmail.com>
2026-06-05 19:24:58 +0530

    4th program

---
+++ DAY_04/To_print_fibonacci_series.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main(){
+    int i,n;
+    int a=0,b=1,c;
+    printf("enter number of terms:");
+    scanf("%d",&n);
+    printf("fibonacci series:");
+    for(i=1;i<=n;i++){
+        printf("%d ",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    return 0;
+}
