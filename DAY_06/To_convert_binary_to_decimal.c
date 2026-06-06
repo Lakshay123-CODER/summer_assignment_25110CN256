2db137e4d1b2b9710257bb7e3a0ca456d152787d
Lakshay Singh <ghanshyamsingh54938@gmail.com>
2026-06-06 20:00:46 +0530

    day_06

---
+++ DAY_06/To_convert_binary_to_decimal.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+int main(){
+    int binary,rem,decimal=0,base=1;
+    printf("enter any binary no:");
+    scanf("%d",&binary);
+    while(binary>0){
+        rem=binary%10;
+        decimal=decimal+rem*base;
+        base=base*2;
+        binary=binary/10;
+    }
+    printf("decimal=%d",decimal);
+    return 0;
+}
---
+++ DAY_06/To_count_sets_bits_in_a_number.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main(){
+    int n,count=0;
+    printf("enter any no:");
+    scanf("%d",&n);
+    while(n>0){
+        if(n%2==1)
+        count++;
+        n=n/2;       
+}
+printf("number of sets bits=%d",count);
+return 0;
+}
